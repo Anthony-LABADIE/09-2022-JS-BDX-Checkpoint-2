2c8b0a67a866ab2f06d044906bdd7a885f41eadc
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-11-18 12:06:02 +0100

    End !!

--- frontend/src/pages/CupcakeDetails.jsx
+++ frontend/src/pages/CupcakeDetails.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function CupcakeDetails() {
-  return <div>fffff</div>;
+  const { id } = useParams();
+
+  const [cupcakeDetail, SetCupcakeDetail] = useState("");
+
+  useEffect(() => {
+    axios.get(`http://localhost:4000/cupcakes/${id}`).then((response) => {
+      SetCupcakeDetail(response.data);
+    });
+  }, [id]);
+  return (
+    <div className="cupcake-container">
+      <div className="cupcake">
+        <img src={cupcakeDetail.url} alt="accessory" />
+        <div className="cream">
+          <div
+            className="cream-1"
+            style={{
+              backgroundColor: cupcakeDetail.color1,
+            }}
+          />
+          <div
+            className="cream-2"
+            style={{
+              backgroundColor: cupcakeDetail.color2,
+            }}
+          />
+          <div
+            className="cream-3"
+            style={{
+              backgroundColor: cupcakeDetail.color3,
+            }}
+          />
+        </div>
+        <div className="bottom">
+          <div className="bottom-in">
+            <div className="face">
+              <div className="eyes">
+                <div className="left-eye" />
+                <div className="right-eye" />
+              </div>
+              <div className="mouth" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="cupcake-name">{cupcakeDetail.name}</div>
+    </div>
+  );
 }
 
 export default CupcakeDetails;
